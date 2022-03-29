634933b3ac7a6d4ae898b0e47be6bd3012ee25e8
Gupta-Saurabh0 <97726837+Gupta-Saurabh0@users.noreply.github.com>
2022-03-29 10:29:44 +0530

    weather app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Temp from './component/weather/temp';
+
+
+const App=()=> {
+  return (
+    <>
+      <Temp/>
+    </>
+  );
+}
+
+export default App;
